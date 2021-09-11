8b4ea53d1c7d8e0a3acb291e11aa5b9c4deb2642
Dan Birt <danbirt@gmail.com>
2021-09-11 14:35:16 -0700

    Fix javascript

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+import WolframNotebookEmbedder from 'wolfram-notebook-embedder';
+var url = "https://www.wolframcloud.com/obj/danbirt/Published/TestCloudDeploy.nb";
+var embedding = WolframNotebookEmbedder.embed('url', document.getElementById('notebookContainer'));

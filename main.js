d0496908910e3309752ab4040845deeaec660990
Dan Birt <danbirt@gmail.com>
2021-09-11 14:41:06 -0700

    Updates

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-import WolframNotebookEmbedder from 'wolfram-notebook-embedder';
 var url = "https://www.wolframcloud.com/obj/danbirt/Published/TestCloudDeploy.nb";
 var embedding = WolframNotebookEmbedder.embed('url', document.getElementById('notebookContainer'));

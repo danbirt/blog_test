df0030e8a69b26aa8ab17798bc89ab6b2aa1979e
Dan Birt <danbirt@gmail.com>
2021-09-11 14:59:54 -0700

    Try cdn for wolfram-embed

--- dist/main.js
+++
@@ -1,3 +0,0 @@
-const WolframNotebookEmbedder = require('wolfram-notebook-embedder');
-var url = "https://www.wolframcloud.com/obj/danbirt/Published/TestCloudDeploy.nb";
-var embedding = WolframNotebookEmbedder.embed('url', document.getElementById('notebookContainer'));

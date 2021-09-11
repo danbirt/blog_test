e7f682485550a830794c623bd1feca2272bd8865
Dan Birt <danbirt@gmail.com>
2021-09-11 14:52:49 -0700

    Try require

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,3 @@
+const WolframNotebookEmbedder = require('wolfram-notebook-embedder');
 var url = "https://www.wolframcloud.com/obj/danbirt/Published/TestCloudDeploy.nb";
 var embedding = WolframNotebookEmbedder.embed('url', document.getElementById('notebookContainer'));
